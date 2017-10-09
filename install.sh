90e9da225ef505a5cd66cdb59ef766b596ba48d1
Ruslan Boitsov <thermatk@thermatk.com>
2017-10-09 23:48:36 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,9 +3,11 @@
 cd /tmp
 wget https://raw.githubusercontent.com/thermatk/VPN-Failsafe-NetworkManager/master/90_vpn_failsafe
 wget https://raw.githubusercontent.com/thermatk/VPN-Failsafe-NetworkManager/master/disableFW.desktop
+rm -f /etc/NetworkManager/dispatcher.d/90_vpn_failsafe
 cp 90_vpn_failsafe /etc/NetworkManager/dispatcher.d
 chmod 700 /etc/NetworkManager/dispatcher.d/90_vpn_failsafe
 chown root:root /etc/NetworkManager/dispatcher.d/90_vpn_failsafe
 
 chmod +x disableFW.desktop
+rm -f /usr/share/applications/disableFW.desktop
 cp disableFW.desktop /usr/share/applications
